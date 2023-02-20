7beb037f4e4d2884efc8251ddfd19fdcb23a410d
hade21 <abdurrohmanmuhammad123@gmail.com>
2023-02-20 13:54:01 +0300

    feat:cofigure dbconnect

--- src/config/dbConnect.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config";
-
-const MONGO_URI = config.MONGO_URI;
-
-export const dbConnect = () => {
-  console.log(MONGO_URI);
-};
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,26 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import config from "./config/config";
-import { dbConnect } from "./config/dbConnect";
 
 function app() {
   const app = express();
-  const PORT = config.PORT;
-  console.log(PORT);
-
-  dbConnect();
+  const PORT: number = config.PORT;
+  const MONGO_URI: string = config.MONGO_URI;
 
   app.use(cors());
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 app();
