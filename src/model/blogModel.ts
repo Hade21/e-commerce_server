52c4ccac696c390db6336d633d9685b6e4f9f907
hade21 <abdurrohmanmuhammad123@gmail.com>
2023-04-10 11:06:29 +0800

    feat: create blogModel

---
+++ src/model/blogModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+
+const blogShema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    numViews: { type: Number, default: 0 },
+    isLiked: { type: Boolean, default: false },
+    isDisliked: { type: Boolean, default: false },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    images: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5DFGqY1xEkoJUpvPRy2ISAIjvAUBvbPHnDQ&usqp=CAU",
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
+
+const Blog = mongoose.model("Blog", blogShema);
+
+export default Blog;
