f2729ad1d916e7cb5a754ae2dab768e67f59aa69
hade21 <abdurrohmanmuhammad123@gmail.com>
2023-05-18 14:51:59 +0800

    feat: create feature delete post and like post

--- src/controller/blogController.ts
+++ src/controller/blogController.ts
@@ -1,7 +1,9 @@
+import { CustomRequest, Payload } from "global";
 import Blog from "../model/blogModel";
 import User from "../model/userModel";
 import { Request, Response } from "express";
 
+//create post
 export const createBlog = async (req: Request, res: Response) => {
   try {
     const newPost = await Blog.create(req.body);
@@ -10,13 +12,94 @@ export const createBlog = async (req: Request, res: Response) => {
     return res.status(400).json(error);
   }
 };
-
+//update post
 export const updateBlog = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const updatedBlog = Blog.findByIdAndUpdate(id, req.body, { new: true });
+    const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
     return res.status(200).json({ message: "Blog updated", updatedBlog });
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+//get all post
+export const getAllBlog = async (req: Request, res: Response) => {
+  try {
+    const posts = await Blog.find();
+    if (posts.length === 0)
+      return res.status(404).json({ message: "No post found" });
+    return res.status(200).json({ message: "Article found", posts });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+//get post by id
+export const getBlogById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const post = await Blog.findById(id);
+    const updatedPost = await Blog.findByIdAndUpdate(
+      id,
+      { numViews: post!.numViews + 1 },
+      { new: true }
+    );
+    return res.status(200).json({ message: "Article found", updatedPost });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+//delete post
+export const deleteBlog = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    await Blog.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Article deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+//like post
+export const likeBlog = async (req: CustomRequest, res: Response) => {
+  const { id: blogId } = req.params;
+  const blog = await Blog.findById(blogId);
+  const { id: loginUserId } = req.user as Payload;
+  const isLiked = blog?.isLiked;
+
+  const alreadyDisliked = blog?.dislikes.find(
+    (userId) => userId.toString() === loginUserId.toString()
+  );
+
+  if (alreadyDisliked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    return res.status(200).json({ blog });
+  } else if (isLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    return res.status(200).json({ blog });
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: loginUserId },
+        isLiked: true,
+      },
+      { new: true }
+    );
+    return res.status(200).json({ blog });
   }
 };
--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -1,10 +1,21 @@
 import express from "express";
-import { createBlog, updateBlog } from "../controller/blogController";
+import {
+  createBlog,
+  deleteBlog,
+  getAllBlog,
+  getBlogById,
+  likeBlog,
+  updateBlog,
+} from "../controller/blogController";
 import { authMiddleware, isAdmin } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/new", authMiddleware, isAdmin, createBlog);
 router.put("/update/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/", getAllBlog);
+router.get("/:id", getBlogById);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
+router.put("/like/:id", authMiddleware, isAdmin, likeBlog);
 
 export default router;
