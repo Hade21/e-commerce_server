eed81080c3c80f649732ae431388107833cd9434
hade21 <abdurrohmanmuhammad123@gmail.com>
2023-05-21 11:36:49 +0800

    feat: create feature get single category

--- src/routes/productCategoryRoute.ts
+++ src/routes/productCategoryRoute.ts
@@ -3,6 +3,7 @@ import {
   createCategory,
   deleteCategory,
   getAllCategory,
+  getSingleCategory,
   updateCategory,
 } from "../controller/productCategoryController";
 import { authMiddleware } from "../middleware/authMiddleware";
@@ -12,6 +13,7 @@ const router = express.Router();
 router.post("/new", authMiddleware, createCategory);
 router.put("/update/:id", authMiddleware, updateCategory);
 router.get("/", authMiddleware, getAllCategory);
+router.get("/:id", authMiddleware, getSingleCategory);
 router.delete("/delete/:id", authMiddleware, deleteCategory);
 
 export default router;
