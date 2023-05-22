9185a693dac0323ddb9f6de3f8ed7856107d6f99
hade21 <abdurrohmanmuhammad123@gmail.com>
2023-05-22 15:04:31 +0800

    feat: add feature add to wishlish endpoint

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { authMiddleware, isSeller } from "../middleware/authMiddleware";
 import {
+  addToWishlist,
   createProduct,
   deleteProduct,
   getAllProducts,
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post("/new", authMiddleware, isSeller, createProduct);
 router.get("/:_id", getProductDetail);
 router.get("/", getAllProducts);
+router.put("/wishlist", authMiddleware, addToWishlist);
 router.put("/:_id", authMiddleware, isSeller, updateProduct);
 router.delete("/:_id", authMiddleware, isSeller, deleteProduct);
 
